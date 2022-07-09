577d1830d7a23f7108a1a918a63da539d1347881
Pathumi Rashmika <sakuni.pathumi@gmail.com>
2022-07-09 12:21:09 +0530

    Add files via upload

---
+++ SriTrava/includes/footer_1.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <footer style="  background-image: linear-gradient(#9610fb,#000000); "id="footer" class="fh5co-bg-color">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <div class="copyright">
+                                    <p><small>&copy; 2021 Sri-Trava <br> All Rights Reserved. <br>
+                                            Designed by <a href="http://freehtml5.co" target="_blank"></a> <br></small></p>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <h3>Company</h3>
+                                        <ul class="link">
+                                            <li><a href="aboutus.php">About Us</a></li>
+                                            <li><a href="hotel.php">Hotels</a></li>
+                                            <li><a href="signup.php">Join With Us</a></li>
+                                            <li><a href="contact.php">Contact Us</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <h3>Our Facilities</h3>
+                                        <ul class="link">
+                                            <li><a href="#">Resturant</a></li>
+                                            <li><a href="#">Bars</a></li>
+                                            <li><a href="#">Pick-up</a></li>
+                                            <li><a href="#">Swimming Pool</a></li>
+                                            <li><a href="#">Spa</a></li>
+                                            <li><a href="#">Gym</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <h3>Subscribe</h3>
+                                        <p>Sed cursus ut nibh in semper. Mauris varius et magna in fermentum. </p>
+                                        <form action="#" id="form-subscribe">
+                                            <div class="form-field">
+                                                <input type="email" placeholder="Email Address" id="email">
+                                                <input type="submit" id="submit" value="Send">
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <ul class="social-icons">
+                                    <li>
+                                        <a href="#"><i class="icon-twitter-with-circle"></i></a>
+                                        <a href="#"><i class="icon-facebook-with-circle"></i></a>
+                                        <a href="#"><i class="icon-instagram-with-circle"></i></a>
+                                        <a href="#"><i class="icon-linkedin-with-circle"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+    </body>
+</html>
---
+++ SriTrava/includes/header.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+ <html class="no-js">
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Luxe &mdash; 100% Free Fully Responsive HTML5 Template by FREEHTML5.co</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FREEHTML5.CO" />
+
+  
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link rel="shortcut icon" href="favicon.ico">
+	
+	<link rel="stylesheet" href="css/superfish.css">
+	
+	<link rel="stylesheet" href="css/bootstrap-datepicker.min.css">
+	
+	<link rel="stylesheet" href="css/cs-select.css">
+	<link rel="stylesheet" href="css/cs-skin-border.css">
+
+	
+	<link rel="stylesheet" href="css/themify-icons.css">
+	
+	<link rel="stylesheet" href="css/flaticon.css">
+	
+	<link rel="stylesheet" href="css/icomoon.css">
+	
+	<link rel="stylesheet" href="css/flexslider.css">
+	
+
+	<link rel="stylesheet" href="css/style.css">
+
+	
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	
+
+</head>
+<body>
+	<div id="fh5co-wrapper">
+	<div id="fh5co-page">
+	<div id="fh5co-header">
+		<header id="fh5co-header-section">
+			<div class="container">
+				<div class="nav-header">
+					<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+					<h1 id="fh5co-logo"><a href="index.html">Luxe</a></h1>
+					<nav id="fh5co-menu-wrap" role="navigation">
+						<ul class="sf-menu" id="fh5co-primary-menu">
+							<li><a href="index.html">Home</a></li>
+							<li>
+								<a href="hotel.html" class="fh5co-sub-ddown">Hotel</a>
+								<ul class="fh5co-sub-menu">
+								 	<li><a href="#">Luxe Hotel</a></li>
+								 	<li><a href="#">Deluxe Hotel</a></li>
+									<li>
+										<a href="#" class="fh5co-sub-ddown">King Hotel</a>
+										<ul class="fh5co-sub-menu">
+											<li><a href="http://freehtml5.co/preview/?item=build-free-html5-bootstrap-template" target="_blank">Build</a></li>
+											<li><a href="http://freehtml5.co/preview/?item=work-free-html5-template-bootstrap" target="_blank">Work</a></li>
+											<li><a href="http://freehtml5.co/preview/?item=light-free-html5-template-bootstrap" target="_blank">Light</a></li>
+											<li><a href="http://freehtml5.co/preview/?item=relic-free-html5-template-using-bootstrap" target="_blank">Relic</a></li>
+											<li><a href="http://freehtml5.co/preview/?item=display-free-html5-template-using-bootstrap" target="_blank">Display</a></li>
+											<li><a href="http://freehtml5.co/preview/?item=sprint-free-html5-template-bootstrap" target="_blank">Sprint</a></li>
+										</ul>
+									</li>
+									<li><a href="#">Five Star Hotel</a></li> 
+								</ul>
+							</li>
+							<li><a class="active" href="services.html">Services</a></li>
+							<li><a href="blog.html">Blog</a></li>
+							<li><a href="contact.html">Contact</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</header>
+		
+	</div>
+	
+
+
+	
+
+	</div>
+	<!-- END fh5co-page -->
+
+	</div>
+	<!-- END fh5co-wrapper -->
+	
+	<!-- Javascripts -->
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<!-- Dropdown Menu -->
+	<script src="js/hoverIntent.js"></script>
+	<script src="js/superfish.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Counters -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Owl Slider -->
+	<!-- // <script src="js/owl.carousel.min.js"></script> -->
+	<!-- Date Picker -->
+	<script src="js/bootstrap-datepicker.min.js"></script>
+	<!-- CS Select -->
+	<script src="js/classie.js"></script>
+	<script src="js/selectFx.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+
+	<script src="js/custom.js"></script>
+
+</body>
+</html>
